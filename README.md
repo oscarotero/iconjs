ea94ebe358e893fef81ea3fa038f5f40d3595bb7
oscarotero <oom@oscarotero.com>
2018-07-01 13:45:29 +0200

    added first code

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # iconjs
-Javascript icon drawer reduced to the minimum expression
+> Javascript icon drawer reduced to the minimum expression
+
+
+This library draw basic svg icons using coordiantes applied to basic lines, circles and rectangles.
+
+For example, to draw a simple `x` (close) icon, you can do:
+
+- Draw a line from top-left to bottom-right
+- Draw other line from bottom-left to top-right
+
+These instructions translated to json are:
+
+```json
+[
+	["l", 0, 0, 1, 1],
+    ["l", 0, 1, 1, 0]
+]
+```
+
+To apply these instructions:
+
+```js
+import draw from './iconjs/drawer.js';
+
+const size = 16;
+const data = [
+	["l", 0, 0, 1, 1],
+    ["l", 0, 1, 1, 0]
+];
+
+const icon = draw(size, data);
+
+document.body.appendChild(icon);
+```
+
+Now, you have a svg icon of `16x16px` in your body.
+
+More examples in [test](test/test.js)
---
+++ src/drawer.js
@@ -0,0 +1,63 @@
+const drawers = { l, r, c };
+
+export default function draw(size, data) {
+    const icon = node('svg', {
+        width: size,
+        height: size,
+        viewBox: `0 0 ${size} ${size}`,
+        preserveAspectRatio: 'none'
+    });
+
+    data.forEach(item => {
+        const name = item.shift();
+        const attrs = item.map(i => Math.round(i * size));
+        const element = drawers[name](...attrs);
+
+        icon.append(element);
+    });
+
+    return icon;
+}
+
+//Draw a line or polyline
+function l(x1, y1, x2, y2) {
+    if (arguments.length > 4) {
+        const points = Array.from(arguments)
+            .map((coord, i, arr) => `${coord},${arr[i + 1]}`)
+            .filter((v, i) => !(i % 2))
+            .join(' ');
+
+        return node('polyline', { points });
+    }
+
+    return node('line', { x1, y1, x2, y2 });
+}
+
+//Draw a circle
+function c(cx, cy, r) {
+    return node('circle', { cx, cy, r });
+}
+
+//Draw a rectangle
+function r(x, y, width, height) {
+    return node('rect', { x, y, width, height });
+}
+
+//Draw a svg node
+function node(name, properties = {}) {
+    const node = document.createElementNS('http://www.w3.org/2000/svg', name);
+
+    Object.keys(properties).forEach(name => {
+        if (name === 'style') {
+            Object.keys(properties[name]).forEach(
+                propName => (node.style[propName] = properties[name][propName])
+            );
+
+            return;
+        }
+
+        node.setAttributeNS(null, name, properties[name]);
+    });
+
+    return node;
+}
